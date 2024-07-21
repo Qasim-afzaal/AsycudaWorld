aa13b6d011d609a26d72f62b45d68989f2b88a37
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-21 23:48:48 +0500

    add main file

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:asycudeworld/src/screens/splash_screen.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
